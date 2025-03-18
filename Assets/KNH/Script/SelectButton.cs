fd22ef82a4652ba95b641914f02d518083640fd5
knhoooo <knh000125@hanyang.ac.kr>
2025-03-19 00:11:11 +0900

    GameManager 추가
    1. 전사 선택 시 전사 프리팹 생성(카메라에는 안보임)
    2. 폴더 정리

--- Assets/KNH/Script/SelectButton.cs
+++ Assets/KNH/Script/SelectButton.cs
@@ -1,9 +1,16 @@
+using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SelectButton : MonoBehaviour
 {
+    private GameObject gameManager;
 
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GameManager");
+    }
     public void StartButton()
     {
         SceneManager.LoadScene("CharacterSelect");//캐릭터 선택 씬 로드
@@ -16,14 +23,17 @@ public class SelectButton : MonoBehaviour
 
     public void WarriorButton()//전사 선택
     {
+        gameManager.GetComponent<GameManager>(). SetClass(0);
         SceneManager.LoadScene("MainGame");//캐릭터 선택 씬 로드
     }
     public void WizardButton()//마법사 선택
     {
+        gameManager.GetComponent<GameManager>().SetClass(1);
         SceneManager.LoadScene("MainGame");//캐릭터 선택 씬 로드
     }
     public void ArcherButton()//궁수 선택
     {
+        gameManager.GetComponent<GameManager>().SetClass(2);
         SceneManager.LoadScene("MainGame");//캐릭터 선택 씬 로드
     }
 }
