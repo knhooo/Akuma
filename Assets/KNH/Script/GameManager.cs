11e6a17cc9de7dde091822b2f54a026699dacc14
knhoooo <knh000125@hanyang.ac.kr>
2025-03-19 17:27:04 +0900

    무한 맵 이동 추가

--- Assets/KNH/Script/GameManager.cs
+++ Assets/KNH/Script/GameManager.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +6,12 @@ public class GameManager : MonoBehaviour
 {
     public int playerClass;//플레이어 직업 0:전사 1:마법사 2:궁수
     [SerializeField] GameObject[] playerPrefabs;//플레이어 프리팹 배열 
+    public static GameManager instance;
+    public GameObject player;
+    [SerializeField] GameObject areaPrefab;
+    GameObject areaObject;
+    //public CinemachineCamera virtualCamera; // 시네머신 가상 카메라
+    [SerializeField] Camera mainCamera;
 
     //플레이어의 직업을 설정하는 메서드
     public void SetClass(int classNo)
@@ -17,15 +24,25 @@ public class GameManager : MonoBehaviour
         return playerClass;
     }
 
+    private void Awake()
+    {
+        instance = this;//초기화   
+    }
+
+    
+
     private void Start()
     {
-        Debug.Log("1");
+        areaObject = Instantiate(areaPrefab) as GameObject;
+        
         if (SceneManager.GetActiveScene().name == "MainGame")//MainGame씬에서
         {
-            Debug.Log("2");
             //플레이어 프리팹 생성
-            Instantiate(playerPrefabs[playerClass], transform.position, Quaternion.identity);
+            player = Instantiate(playerPrefabs[playerClass], transform.position, Quaternion.identity);
         }
+        areaObject.transform.SetParent(player.transform, false);
+        mainCamera.transform.SetParent(player.transform, false);
+
     }
 
 }
