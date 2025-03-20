8ba772a48a91b95078f6a028f612253e04d0c2cd
minji25 <zakijeong917@naver.com>
2025-03-20 17:20:16 +0900

    work

--- Assets/minji/Water_Scripts/WP.cs
+++ Assets/minji/Water_Scripts/WP.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class WaterPlayer : MonoBehaviour
+public class WP : MonoBehaviour
 {
     public float wmoveSpeed = 3f; //움직이는 속도
     public float wspeedUp = 5f; //증가된 속도
@@ -11,7 +11,8 @@ public class WaterPlayer : MonoBehaviour
     public int wPower = 10; //주문력 
     public int wHP = 100; //체력 (20씩 증가)
     public int wLevel = 1; //레벨
-    public int wEx = 1; //레벨
+    public int wEx = 0; //경험치
+
 
 
 
@@ -25,41 +26,41 @@ public class WaterPlayer : MonoBehaviour
     void Start()
     {
         ani = GetComponent<Animator>(); //애니메이션 가져오기
-        StartCoroutine(CheckConditionCoroutine());
+        //StartCoroutine(CheckConditionCoroutine());
     }
 
-    IEnumerator CheckConditionCoroutine()
-    {
-        while (true) // 계속 반복
-        {
-            if (Level>=5)
-            {
-                StartCoroutine(skill1());
-                yield break;
-            }
-            yield return new WaitForSeconds(1f); // 1초마다 검사
-        }
-    }
+    //IEnumerator CheckConditionCoroutine()
+    //{
+    //    while (true) // 계속 반복
+    //    {
+    //        if (wLevel >= 5)
+    //        {
+    //            StartCoroutine(skill1());
+    //            yield break;
+    //        }
+    //        yield return new WaitForSeconds(1f); // 1초마다 검사
+    //    }
+    //}
 
     void Update()
     {
         //방향키에 따른 물마법사 x, y좌표
-        float moveX = moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
-        float moveY = moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        float moveX = wmoveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float moveY = wmoveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
 
 
         //좌우 이동
-        if (Input.GetAxis("Horizontal")<=-0.2) //왼쪽으로 이동할 때
+        if (Input.GetAxis("Horizontal") <= -0.2) //왼쪽으로 이동할 때
         {
             ani.SetBool("walk", true); //walk 모션 활성
             transform.localScale = new Vector3(-1f, 1f, 1f); //캐릭터 좌우반전
 
-            if(Input.GetKey(KeyCode.LeftShift)) //왼쪽으로 이동하면서 Shift누를 때
+            if (Input.GetKey(KeyCode.LeftShift)) //왼쪽으로 이동하면서 Shift누를 때
             {
                 ani.SetBool("walk", false); //걷는 모션 비활성화
                 ani.SetBool("surf", true); //대쉬 모션 활성화
 
-               wmoveSpeed = wspeedUp; //스피드 증가
+                wmoveSpeed = wspeedUp; //스피드 증가
             }
             else //Shift를 땔 때
             {
@@ -82,7 +83,7 @@ public class WaterPlayer : MonoBehaviour
                 ani.SetBool("sp_atk", false);
             }
         }
-        else if (Input.GetAxis("Horizontal")>=0.2) //오른쪽으로 이동할 때
+        else if (Input.GetAxis("Horizontal") >= 0.2) //오른쪽으로 이동할 때
         {
             ani.SetBool("walk", true); //걷는 모션 활성화
             transform.localScale = new Vector3(1f, 1f, 1f); //캐릭터 오른쪽 모습
@@ -92,7 +93,7 @@ public class WaterPlayer : MonoBehaviour
                 ani.SetBool("walk", false); //걷는 모션 비활성화
                 ani.SetBool("surf", true); //대쉬 모션 활성화
 
-               wmoveSpeed = wspeedUp; //스피드 증가
+                wmoveSpeed = wspeedUp; //스피드 증가
             }
             else
             {
@@ -104,7 +105,7 @@ public class WaterPlayer : MonoBehaviour
             if (Input.GetMouseButtonDown(0)) //오른쪽으로 이동하면서 마우스 좌클릭
             {
                 ani.SetBool("sp_atk", true); //공격 모션 활성화
-                GameObject go=Instantiate(waterbullet, pos1.position, Quaternion.identity); //pos1에서 미사일 발사
+                GameObject go = Instantiate(waterbullet, pos1.position, Quaternion.identity); //pos1에서 미사일 발사
                 Destroy(go, 5); //5초 뒤 삭제
             }
             else
@@ -114,13 +115,13 @@ public class WaterPlayer : MonoBehaviour
 
 
         }
-        else if(Input.GetAxis("Horizontal") == 0.0f) //멈춰있을 때
+        else if (Input.GetAxis("Horizontal") == 0.0f) //멈춰있을 때
         {
             ani.SetBool("walk", false); //걷는 모션 비활성화
         }
 
         //Shift누를 때
-        if(Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             ani.SetBool("surf", true);
             wmoveSpeed = wspeedUp; //스피드 증가
@@ -132,9 +133,9 @@ public class WaterPlayer : MonoBehaviour
         }
 
         //마우스 좌클릭
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            if(transform.localScale.x==1f)
+            if (transform.localScale.x == 1f)
             {
                 transform.localScale = new Vector3(1f, 1f, 1f);
                 ani.SetBool("sp_atk", true);
@@ -148,7 +149,7 @@ public class WaterPlayer : MonoBehaviour
                 GameObject go = Instantiate(waterbullet, pos2.position, Quaternion.identity);
                 Destroy(go, 5);
             }
-            
+
         }
         else
         {
@@ -158,7 +159,7 @@ public class WaterPlayer : MonoBehaviour
         //캐릭터 좌표
         transform.Translate(moveX, moveY, 0);
 
-        if(HP==0)
+        if (wHP == 0)
         {
             ani.SetBool("death", true);
         }
@@ -167,7 +168,7 @@ public class WaterPlayer : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Enemy"))
+        if (collision.CompareTag("Enemy"))
         {
             ani.SetBool("takehit", true);
             wHP -= 10;
@@ -182,61 +183,61 @@ public class WaterPlayer : MonoBehaviour
         }
     }
 
-    public void ExUp(int ex)
-    {
-        wEx += ex;
+    //public void ExUp(int ex)
+    //{
+    //    wEx += ex;
 
-        if(wEx >= 50)
-        {
-            Level =2;
-        }
-        else if(wEx >= 100&& wEx < 200)
-        {
-            Level = 3;
-        }
-        else if(wEx >= 200&& wEx < 300)
-        {
-            Level = 4;
-        }
-        else if(wEx >= 300)
-        {
-            Level = 5;
-        }
-    }
+    //    if (wEx >= 50)
+    //    {
+    //        wLevel = 2;
+    //    }
+    //    else if (wEx >= 100 && wEx < 200)
+    //    {
+    //        wLevel = 3;
+    //    }
+    //    else if (wEx >= 200 && wEx < 300)
+    //    {
+    //        wLevel = 4;
+    //    }
+    //    else if (wEx >= 300)
+    //    {
+    //        wLevel = 5;
+    //    }
+    //}
 
-    IEnumerator skill1()
-    {
-        //공격주기
-        float attackRate = 3;
-        //발사체 생성갯수
-        int count = 5;
-        //발사체 사이의 각도
-        float intervalAngle = 360 / count;
-        //가중되는 각도(항상 같은 위치로 발사하지 않도록 설정
-        float weightAngle = 0f;
-        while (true)
-        {
-            for (int i = 0; i < count; ++i)
-            {
-                //발사체 생성
-                GameObject clone = Instantiate(waterskill1, transform.position, Quaternion.identity);
+    //IEnumerator skill1()
+    //{
+    //    //공격주기
+    //    float attackRate = 3;
+    //    //발사체 생성갯수
+    //    int count = 5;
+    //    //발사체 사이의 각도
+    //    float intervalAngle = 360 / count;
+    //    //가중되는 각도(항상 같은 위치로 발사하지 않도록 설정
+    //    float weightAngle = 0f;
+    //    while (true)
+    //    {
+    //        for (int i = 0; i < count; ++i)
+    //        {
+    //            //발사체 생성
+    //            GameObject clone = Instantiate(waterskill1, transform.position, Quaternion.identity);
 
-                //발사체 이동 방향(각도)
-                float angle = weightAngle + intervalAngle * i;
-                //발사체 이동 방향(벡터)
-                //Cos(각도)라디안 단위의 각도 표현을 위해 pi/180을 곱함
-                float x = Mathf.Cos(angle * Mathf.Deg2Rad);
-                //sin(각도)라디안 단위의 각도 표현을 위해 pi/180을 곱함
-                float y = Mathf.Sin(angle * Mathf.Deg2Rad);
+    //            //발사체 이동 방향(각도)
+    //            float angle = weightAngle + intervalAngle * i;
+    //            //발사체 이동 방향(벡터)
+    //            //Cos(각도)라디안 단위의 각도 표현을 위해 pi/180을 곱함
+    //            float x = Mathf.Cos(angle * Mathf.Deg2Rad);
+    //            //sin(각도)라디안 단위의 각도 표현을 위해 pi/180을 곱함
+    //            float y = Mathf.Sin(angle * Mathf.Deg2Rad);
 
-                //발사체 이동 방향 설정
-                clone.GetComponent<Water>().Move(new Vector2(x, y));
-            }
-            //발사체가 생성되는 시작 각도 설정을 위한변수
-            weightAngle += 1;
-            //3초마다 미사일 발사
-            yield return new WaitForSeconds(attackRate);
-        }
+    //            //발사체 이동 방향 설정
+    //            clone.GetComponent<Water>().Move(new Vector2(x, y));
+    //        }
+    //        //발사체가 생성되는 시작 각도 설정을 위한변수
+    //        weightAngle += 1;
+    //        //3초마다 미사일 발사
+    //        yield return new WaitForSeconds(attackRate);
+    //    }
 
-    }
+    //}
 }
