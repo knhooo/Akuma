10aa2e7a8a9382b6f2133834afa29949b2cb266e
김승연 <98165542+Decricia@users.noreply.github.com>
2025-03-21 16:18:47 +0900

    아처 화살날리기
    1.가장 가까운적 찾아날아가기(tag)

--- Assets/K_Folder/Scripts/ArcherAutoShooter.cs
+++ Assets/K_Folder/Scripts/ArcherAutoShooter.cs
@@ -1,45 +1,17 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class ArcherAnim : Player
+public class ArcherAutoShooter : MonoBehaviour
 {
-    private Animator animator;
-    private Rigidbody2D rb;
-
     public GameObject arrowPrefab;
     public Transform firePoint;
     public float arrowSpeed = 10f;
     public float shootInterval = 2f;
 
-    void Awake()
-    {
-        animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-    }
-
     void Start()
     {
-        rb.gravityScale = 0;
-        StartCoroutine(AutoShoot()); // ï¿½Úµï¿½ ï¿½ß»ï¿½ ï¿½ï¿½ï¿½ï¿½
-    }
-
-    void Update()
-    {
-        HandleMovement();
-    }
-
-    void HandleMovement()
-    {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-        rb.linearVelocity = new Vector2(moveX * speed, moveY * speed);
-
-        animator.SetBool("isMoving", moveX != 0 || moveY != 0);
-
-        if (moveX > 0)
-            transform.localScale = new Vector3(1, 1, 1);
-        else if (moveX < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
+        StartCoroutine(AutoShoot());
     }
 
     IEnumerator AutoShoot()
@@ -59,14 +31,14 @@ public class ArcherAnim : Player
     {
         GameObject[] monsters = GameObject.FindGameObjectsWithTag("Monster");
         Transform closest = null;
-        float minDist = Mathf.Infinity;
+        float minDistance = Mathf.Infinity;
 
         foreach (GameObject monster in monsters)
         {
             float dist = Vector2.Distance(transform.position, monster.transform.position);
-            if (dist < minDist)
+            if (dist < minDistance)
             {
-                minDist = dist;
+                minDistance = dist;
                 closest = monster.transform;
             }
         }
@@ -81,16 +53,14 @@ public class ArcherAnim : Player
         GameObject arrow = Instantiate(arrowPrefab, firePoint.position, Quaternion.identity);
         arrow.transform.localScale = Vector3.one;
 
+        // È­»ì ¹æÇâ º¸Á¤ (Sprite°¡ ¿À¸¥ÂÊÀÌ¸é angle ±×´ë·Î / À§ÂÊÀÌ¸é -90f µî)
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        arrow.transform.rotation = Quaternion.Euler(0, 0, angle); // Spriteï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½
+        arrow.transform.rotation = Quaternion.Euler(0f, 0f, angle); // ¼öÁ¤ °¡´É
 
         Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
         if (rb != null)
         {
             rb.linearVelocity = direction * arrowSpeed;
         }
-
-        // ï¿½Ö´Ï¸ï¿½ï¿½Ì¼ï¿½ Æ®ï¿½ï¿½ï¿½ï¿½ ï¿½Ê¿ï¿½ï¿½Ï¸ï¿½ ï¿½ï¿½ï¿½
-        // animator.SetTrigger("2Attack");
     }
 }
