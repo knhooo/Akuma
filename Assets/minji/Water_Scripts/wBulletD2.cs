b947d04382495244d4cd952e725083d0576be700
minji25 <zakijeong917@naver.com>
2025-03-25 15:15:42 +0900

    work

--- Assets/minji/Water_Scripts/wBulletD2.cs
+++ Assets/minji/Water_Scripts/wBulletD2.cs
@@ -14,6 +14,7 @@ public class wBulletD2 : MonoBehaviour
     {
         if (collision.CompareTag("Monster"))
         {
+            wSoundManager.instance.tWater();
             Instantiate(wbd, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
