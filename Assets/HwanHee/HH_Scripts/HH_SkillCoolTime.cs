7029c0478d60dcdefafe74db8df47c5a53e6d0fd
chlghksgml01 <58538862+chlghksgml01@users.noreply.github.com>
2025-03-28 20:54:13 +0900

    코드 수정

--- Assets/HwanHee/HH_Scripts/HH_SkillCoolTime.cs
+++ Assets/HwanHee/HH_Scripts/HH_SkillCoolTime.cs
@@ -3,19 +3,40 @@ using UnityEngine.UI;
 
 public class HH_SkillCoolTime : MonoBehaviour
 {
+    [SerializeField] string skillStr;
     Image Gage;
     Player player;
     float val = 0;
+    float skillCoolTime;
+    float skillCoolTimer;
 
-    private void Awake()
+    private void Start()
     {
         Gage = GetComponent<Image>();
         player = GameObject.FindWithTag("Player").GetComponent<Player>();
+
+        if (skillStr == "Skill" || skillStr == "skill")
+        {
+            skillCoolTime = player.SkillCoolTime;
+        }
+        else if (skillStr == "Dash" || skillStr == "dash")
+        {
+            skillCoolTime = player.DashCoolTime;
+        }
     }
 
     void Update()
     {
-        val = Mathf.InverseLerp(player.SkillCoolTime, 0f, player.SkillCoolTimer);
+        if (skillStr == "Skill" || skillStr == "skill")
+        {
+            skillCoolTimer = player.SkillCoolTimer;
+        }
+        else if (skillStr == "Dash" || skillStr == "dash")
+        {
+            skillCoolTimer = player.DashCoolTimer;
+        }
+
+        val = Mathf.InverseLerp(skillCoolTime, 0f, skillCoolTimer);
         Gage.fillAmount = val;
     }
 }
